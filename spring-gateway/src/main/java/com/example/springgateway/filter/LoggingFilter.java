aba805e8ffb4a1c1d8364bd08d6792597556d323
cgw981 <cgw981@naver.com>
2022-12-20 22:41:13 +0900

    Logging Filter -2

--- spring-gateway/src/main/java/com/example/springgateway/filter/LoggingFilter.java
+++ spring-gateway/src/main/java/com/example/springgateway/filter/LoggingFilter.java
@@ -38,7 +38,7 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
                     log.info("Logging post filter end :: response code -> {}", response.getStatusCode());
                 }
             }));
-        }, Ordered.HIGHEST_PRECEDENCE);
+        }, Ordered.LOWEST_PRECEDENCE);
 
         return filter;
     }
