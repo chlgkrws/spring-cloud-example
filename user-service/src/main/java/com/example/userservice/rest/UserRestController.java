94812ccf0e9f5a6cfef4be9f189f547c80c4c2bf
cgw981 <cgw981@naver.com>
2023-03-05 21:15:05 +0900

    users - spring cloud config 연동 2 - ~12:34

--- user-service/src/main/java/com/example/userservice/rest/UserRestController.java
+++ user-service/src/main/java/com/example/userservice/rest/UserRestController.java
@@ -32,7 +32,11 @@ public class UserRestController {
 
     @GetMapping("/health-check")
     public String status() {
-        return String.format("It's Working in User Service on port %s", env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port) = " + env.getProperty("local.server.port")
+                + ", port(server.port) = " + env.getProperty("server.port")
+                + ", token secret = " + env.getProperty("token.secret")
+                + ", token expiration time = " + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
