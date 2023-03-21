cfe16afbb5fd57fdfb389b6e96ab56bc3b29fae9
cgw981 <cgw981@naver.com>
2023-03-21 21:47:37 +0900

    End spring gateway config

--- spring-gateway/src/main/java/com/example/springgateway/SpringGatewayApplication.java
+++ spring-gateway/src/main/java/com/example/springgateway/SpringGatewayApplication.java
@@ -1,14 +1,25 @@
 package com.example.springgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGatewayApplication.class, args);
+    }
+
+    /**
+     * 엑츄에이터(의존성) 기능 중 httptrace 옵션을 사용하기 위한 Bean 등록
+     */
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
